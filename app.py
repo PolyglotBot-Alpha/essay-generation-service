33f86733b6dc0e8d2dc282597e9a25bcfb428b6d
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2024-03-20 16:05:02 -0400

    Eureka Client Register
    Register eureka client service via eureka server

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from py_eureka_client import eureka_client
 
 app = Flask(__name__)
 
+# Eureka server URL
+eureka_server = "http://localhost:8761/eureka/"
+
+# Application name, host, and port
+app_name = "flask-eureka-client"
+app_host = "localhost"
+app_port = 5000
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def hello_world():
+    return 'Hello, World from Flask Eureka Client!'
 
 if __name__ == '__main__':
-    app.run()
+    # Register the application with Eureka
+    eureka_client.init(eureka_server=eureka_server,
+                       app_name=app_name,
+                       instance_port=app_port,
+                       instance_host=app_host)
+    app.run(host=app_host, port=app_port)

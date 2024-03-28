fdb37cc2a93a774f5965c7fac96038f8b9bf8e17
Yuqi Guo <131561736+YuqiGuo105@users.noreply.github.com>
2024-03-27 22:33:11 -0400

    Reformat and Bug fix
    1. Encapsulate the OpenAI method into a Class
    2. Import .env file to get the OpenAI's API_KEY

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from py_eureka_client import eureka_client
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
@@ -7,13 +9,27 @@ app = Flask(__name__)
 eureka_server = "http://localhost:8761/eureka/"
 
 # Application name, host, and port
-app_name = "flask-eureka-client"
+app_name = "AI-Content-Generation"
 app_host = "localhost"
 app_port = 5000
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Flask Eureka Client!'
+
+
+@app.route('/generate_essay')
+def essay_generation():
+    api_key = os.getenv('API_KEY')
+    if api_key is not None:
+        return api_key
+    else:
+        return "API key not found. Please set the API_KEY environment variable."
+
 
 if __name__ == '__main__':
     # Register the application with Eureka
